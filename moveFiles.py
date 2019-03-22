407b05e86220e9b025de0db74399797a6c7bcbe0
DESKTOP-ABM275T\Alex Williamson <alexwilliamson123@gmail.com>
2019-03-22 13:34:49 -0700

    added some imports

--- moveFiles.py
+++ moveFiles.py
@@ -1,43 +1,51 @@
+import os
+import pandas as pd
+import glob
+import random
+from sklearn.model_selection import train_test_split
+import shutil
 
-path_to_all = os.path.join('data', 'videos', 'all')
+def moveFiles():
 
-# load files names
-filenames = glob.glob(os.path.join(path_to_all, '*.avi'))
+    path_to_all = os.path.join('data', 'videos', 'all')
 
-classes = ['normal', 'insert', 'dropped', 'compressed', 'black']
-fileclasses = [random.choice(classes) for f in filenames]
+    # load files names
+    filenames = glob.glob(os.path.join(path_to_all, '*.avi'))
 
-df = pd.DataFrame({'filenames': filenames, 'classes': fileclasses})
+    classes = ['normal', 'insert', 'dropped', 'compressed', 'black']
+    fileclasses = [random.choice(classes) for f in filenames]
 
-train = df.reset_index().groupby('classes').apply(lambda x: x.sample(frac=0.8)).reset_index(drop=True).set_index('index')                  
-test = df.drop(train.index)
+    df = pd.DataFrame({'filenames': filenames, 'classes': fileclasses})
 
-trainPath = os.path.join('data', 'videos', 'train')
-testPath = os.path.join('data', 'videos', 'test')
+    train = df.reset_index().groupby('classes').apply(lambda x: x.sample(frac=0.8)).reset_index(drop=True).set_index('index')                  
+    test = df.drop(train.index)
 
-# Extract images
-if (os.path.isdir(trainPath)):
-    shutil.rmtree(trainPath)
-os.makedirs(trainPath)
+    trainPath = os.path.join('data', 'videos', 'train')
+    testPath = os.path.join('data', 'videos', 'test')
 
-if (os.path.isdir(testPath)):
-    shutil.rmtree(testPath)
-os.makedirs(testPath)
-        
-for index, row in train.iterrows():
-    f = row.filenames
-    videoName = f.split(os.path.sep)[-1]
-    c = row.classes
-    p = os.path.join(trainPath, c)
-    os.makedirs(p, exist_ok = True) #if it exits do nothing
-    outPath = os.path.join(p, videoName)
-    shutil.copyfile(f, outPath) # copy from all to the correct folder
+    # Extract images
+    if (os.path.isdir(trainPath)):
+        shutil.rmtree(trainPath)
+    os.makedirs(trainPath)
 
-for index, row in test.iterrows():
-    f = row.filenames
-    videoName = f.split(os.path.sep)[-1]
-    c = row.classes
-    p = os.path.join(testPath, c)
-    os.makedirs(p, exist_ok = True) #if it exits do nothing
-    outPath = os.path.join(p, videoName)
-    shutil.copyfile(f, outPath) # copy from all to the correct folder
+    if (os.path.isdir(testPath)):
+        shutil.rmtree(testPath)
+    os.makedirs(testPath)
+            
+    for index, row in train.iterrows():
+        f = row.filenames
+        videoName = f.split(os.path.sep)[-1]
+        c = row.classes
+        p = os.path.join(trainPath, c)
+        os.makedirs(p, exist_ok = True) #if it exits do nothing
+        outPath = os.path.join(p, videoName)
+        shutil.copyfile(f, outPath) # copy from all to the correct folder
+
+    for index, row in test.iterrows():
+        f = row.filenames
+        videoName = f.split(os.path.sep)[-1]
+        c = row.classes
+        p = os.path.join(testPath, c)
+        os.makedirs(p, exist_ok = True) #if it exits do nothing
+        outPath = os.path.join(p, videoName)
+        shutil.copyfile(f, outPath) # copy from all to the correct folder
